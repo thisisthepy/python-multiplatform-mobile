6bc79b13470a5de9faa56297e757a37c8a30259d
BangJunSeok <bangjunseok2005@naver.com>
2025-12-20 14:04:26 +0900

    Feat: Further Implementation for PyType (Temporal Commit)

--- python-multiplatform/src/commonMain/kotlin/python/multiplatform/ffi/PyObject.kt
+++ python-multiplatform/src/commonMain/kotlin/python/multiplatform/ffi/PyObject.kt
@@ -3,73 +3,91 @@ package python.multiplatform.ffi
 import python.multiplatform.ffi.exceptions.PyException
 import python.multiplatform.ref.PyAutoCloseable
 import python.native.ffi.NativePointer
+import python.native.ffi.PyErr_Occurred
+import python.native.ffi.PyLong_AsLongLong
+import python.native.ffi.PyLong_FromLongLong
+import python.native.ffi.PyObject_DelAttrString
+import python.native.ffi.PyObject_GetAttrString
+import python.native.ffi.PyObject_SetAttrString
+import python.native.ffi.PyObject_Str
+import python.native.ffi.PyObject_Type
+import python.native.ffi.PyUnicode_AsUTF8
+import python.native.ffi.Py_DecRef
+import python.native.ffi.Py_IncRef
+import python.native.ffi.memScoped
 
 
 open class PyObject(val pointer: NativePointer, borrowed: Boolean): PyAutoCloseable(pointer) {
-    override fun clean() {
-        TODO("Not yet implemented")
-    }
-}
-/*
-    //init {
-    //    if (!borrowed) {
-            //PyIncRef(pointer)
-            // TODO: PyIncRef을 사용하는게 적절한 선택일까?
-    //    }
-    //}
-    // TODO: Temporary commented due to the error: Expected declaration cannot have a body.
-    // TODO: Move this to the actual implementation.
 
-    val pointer: NativePointer
-    protected val typePointer: NativePointer = lazy { PyType_GetType(this.pointer) }
-    val Type: PyType = lazy { PyType(typePointer!!) }
+    init {
+        if (borrowed) {
+            Py_IncRef(pointer)
+            // TODO: PyIncRef을 사용하는게 적절한 선택일까?
+        }
+    }
+
+    //    protected val typePointer: NativePointer = lazy { PyType_GetType(this.pointer) } // TODO: PyType_GetType 함수 제작 | 함수 body를 어디에 작성...? 일단 EmbedAPI.kt에 작성
+    // protected val typePointer: NativePointer = lazy { PyObject_Type(this.pointer) } // TODO: PyObject_Type가 위의 PyType_GetType의 역할이 맞는지 확인 | 이 instance가 사라지면 이 변수가 가리키는 Object의 RefCount를 1 줄여주어야 함
+    protected val typePointer: NativePointer by lazy { PyObject_Type(this.pointer).let {
+        // TODO: catch python side error **Do Not cause error in kotlin**
+        if (it == null) throw NullPointerException("Failed to get type pointer")
+        else it
+    }} // TODO: Null check 하기
+//    val Type: PyType = lazy { PyType(typePointer!!) }
+    val type: PyType by lazy { PyType(typePointer) } // TODO: borrowed를 바꿀 수 있게 할지 결정
 
     fun incRef() {
         println("hi")
+        // TODO: 예외 처리 추가
+        Py_IncRef(pointer)
     }
 
     fun decRef() {
         println("hi")
+        // TODO: 예외 처리 추가
+        Py_DecRef(pointer)
     }
 
-    @Throws(PyException::class)
-    fun getAttr(name: String): PyObject {
-        val attr = PyObject_GetAttrString(pointer, name)
-        if (attr == null) {
-            throw PyException("Attribute '$name' not found")
-        }
-        return PyObject(attr, false)
-    }
+//    @Throws(PyException::class)
+//    fun getAttr(name: String): PyObject {
+//        val attr = PyObject_GetAttrString(pointer, name)
+//        if (attr == null) {
+//            throw PyException("Attribute '$name' not found")
+//        }
+//        return PyObject(attr, false)
+//    }
 
     fun getAttrOrNull(name: String): PyObject? {
         val attr = PyObject_GetAttrString(pointer, name)
         return if (attr != null) PyObject(attr, false) else null
     }
 
-    @Throws(PyException::class)
-    fun setAttr(name: String, value: PyObject) {
-        if (PyObject_SetAttrString(pointer, name, value.pointer) != 0) {
-            throw PyException("Failed to set attribute '$name'")
-        }
-    }
+//    @Throws(PyException::class)
+//    fun setAttr(name: String, value: PyObject) {
+//        if (PyObject_SetAttrString(pointer, name, value.pointer) != 0) {
+//            throw PyException("Failed to set attribute '$name'")
+//        }
+//    }
 
     fun setAttrOrNull(name: String, value: PyObject?) {
-        PyObject_SetAttrString(pointer, name, value?.pointer)
+        value?.pointer?.let { PyObject_SetAttrString(pointer, name, it) }
     }
 
-    @Throws(PyException::class)
-    fun delAttr(name: String) {
-        if (PyObject_DelAttrString(pointer, name) != 0) {
-            throw PyException("Failed to delete attribute '$name'")
-        }
-    }
+//    @Throws(PyException::class)
+//    fun delAttr(name: String) {
+//        if (PyObject_DelAttrString(pointer, name) != 0) {
+//            throw PyException("Failed to delete attribute '$name'")
+//        }
+//    }
 
     fun delAttrOrNull(name: String) {
         PyObject_DelAttrString(pointer, name)
     }
 
     override fun toString(): String {
-        return PyObject_GetStr(pointer)
+        // TODO: PyObject_Str, PyUnicode_AsUTF8 return value가 null인 경우 처리
+        return PyUnicode_AsUTF8(PyObject_Str(pointer)!!)!!
+//        return PyObject_GetStr(pointer)
     }
 
     override fun hashCode(): Int {
@@ -84,23 +102,31 @@ open class PyObject(val pointer: NativePointer, borrowed: Boolean): PyAutoClosea
         return false
     }
 
-    operator fun invoke(arg0: PyObject): PyObject {
-        return PyObject_CallObject(pointer, arg0)
+    override fun clean() {
+        Py_DecRef(typePointer)
     }
 
-    operator fun invoke(arg0: PyObject, arg1: PyObject): PyObject {
-        return PyObject_CallObject(pointer, arg0, arg1)
-    }
+    // TODO: 밑에 세 함수 수정 (return type 불일치 등)
+//    operator fun invoke(arg0: PyObject): PyObject {
+//        return PyObject_CallObject(pointer, arg0)
+//    }
+//
+//    operator fun invoke(arg0: PyObject, arg1: PyObject): PyObject {
+//        return PyObject_CallObject(pointer, arg0, arg1) // TODO: PyObject_CallObject 함수 매개변수는 2개 아닌가...?
+//    }
+//
+//    //...
+//
+//    operator fun invoke(vararg args: PyObject): PyObject {
+//        return PyObject_CallObject(pointer, args)  // TODO: 이거는 변수 하나로 잡히던가? 아님 여러개인가?
+//        // 리스트로 들어오는 거였던가?
+//    }
 
-    //...
-
-    operator fun invoke(vararg args: PyObject): PyObject {
-        return PyObject_CallObject(pointer, args)  // TODO: 이거는 변수 하나로 잡히던가? 아님 여러개인가?
-                                                    // 리스트로 들어오는 거였던가?
-    }//    actual fun pyLongFromLong(arg0: Long): Long {
-//        if (!isInitialized) return -1
+//    actual fun pyLongFromLong(arg0: Long): Long {
+//        if (!Python3.isInitialized) return -1
 //        memScoped {
-//            val pyLong = PyLong_FromLong(arg0)
+////            val pyLong = PyLong_FromLong(arg0) // TODO: PyLong_FromLong을 PyLong_FromLongLong으로 교체
+//            val pyLong = PyLong_FromLongLong(arg0)
 //            if (pyLong == null) {
 //                throw IllegalStateException("Python long from long failed")
 //            }
@@ -109,10 +135,11 @@ open class PyObject(val pointer: NativePointer, borrowed: Boolean): PyAutoClosea
 //    }
 //
 //    actual fun pyLongAsLong(arg0: Long): Long {
-//        if (!isInitialized) return -1
+//        if (!Python3.isInitialized) return -1
 //        memScoped {
 //            val restoredPyObj: CValuesRef<_object>? = arg0.toCPointer()
-//            val ktLong = PyLong_AsLong(restoredPyObj)
+////            val ktLong = PyLong_AsLong(restoredPyObj) // TODO: PyLong_AsLong을 PyLong_AsLongLong으로 교체
+//            val ktLong = PyLong_AsLongLong(restoredPyObj)
 //            if (ktLong == -1L && PyErr_Occurred() != null) {
 //                throw IllegalStateException("Python long as long failed")
 //            }
@@ -120,4 +147,3 @@ open class PyObject(val pointer: NativePointer, borrowed: Boolean): PyAutoClosea
 //        }
 //    }
 }
-*/
--- python-multiplatform/src/commonMain/kotlin/python/multiplatform/ffi/PyType.kt
+++ python-multiplatform/src/commonMain/kotlin/python/multiplatform/ffi/PyType.kt
@@ -1,4 +1,91 @@
 package python.multiplatform.ffi
 
-class PyType {
+import python.multiplatform.ffi.types.collections.PyDict
+import python.multiplatform.ffi.types.iteration.PyIterator
+import python.native.ffi.NativePointer
+import python.native.ffi.PyObject_GetAttr
+import python.native.ffi.PyObject_GetAttrString
+import python.native.ffi.PyObject_Type
+import python.native.ffi.PyTuple_GetItem
+import python.native.ffi.PyTuple_Size
+import python.native.ffi.PyType_GetName
+import python.native.ffi.PyUnicode_AsUTF8
+import python.native.ffi.Py_DecRef
+
+
+// TODO: PyObject를 PyTypeObject 자리에 넣어도 될까?
+// TODO: 어떤 instance의 type을 알고 싶을 때 PyObject_Type 사용?
+// TODO: __slots__를 사용하는 type을 고려 할 필요가 있을까?
+class PyType(pointer: NativePointer): PyObject(pointer, false) {
+    val name: String by lazy {
+        PyUnicode_AsUTF8(PyType_GetName(pointer)!!)!!
+    }
+
+    val baseType: PyType by lazy {
+        PyType(PyObject_GetAttrString(pointer, "__base__")!!)
+    } // TODO: 어떻게 가져올지 알아보기. 현재 직접적으로 부모 클래스에 접근하는 API는 없어보임.
+//    val baseTypes: List<PyType> // TODO: PyObject_GetAttrString로 가져오기
+//    val mro: List<PyType>
+//    val dict: PyDict // TODO: PyType_GetDict 사용
+
+    init {
+        if (!isPyTypeObject()) throw IllegalArgumentException("Object is not a type")
+
+        var temp: MutableList<PyType> = mutableListOf<PyType>()
+        // TODO: PyObject_GetAttrString return값이 Tuple인지도 확인 해야할까?
+        val basesPyObject: NativePointer = PyObject_GetAttrString(pointer, "__bases__").let {it ?: throw NullPointerException("Failed to get base types")}
+        val basesSize: Long = PyTuple_Size(basesPyObject).let {
+            if (it == -1L) throw IllegalStateException("Failed to get base types size")
+            else it
+        }
+        for (i in 0 until basesSize) {
+            // TODO: type check 필요
+//            temp.add(PyType(PyTuple_GetItem(basesPyObject, i)!!, true))
+        }
+    }
+
+//    fun isSubtypeOf(other: PyType): Boolean {
+//        // TODO: PyType_IsSubtype(PyTypeObject *a, PyTypeObject *b) 사용
+//    }
+//
+//    fun cast(obj: PyObject): PyObject {
+//
+//    }
+//
+//    fun isInstance(obj: PyObject): Boolean {
+//        // TODO: PyObject_IsInstance 이용
+//    }
+//
+//    fun getIterator(): PyIterator {
+//        // TODO: 이건 무슨 함수인가?
+//    }
+//
+//    operator fun invoke(args: Array<>): PyObject {
+//
+//    }
+//
+//    fun __new__(): PyObject {
+//
+//    }
+//
+//    fun __init__(obj: PyObject, args: Array<>) {
+//
+//    }
+
+    private fun isPyTypeObject(): Boolean {
+        val pyTypeObject: NativePointer? = PyObject_Type(pointer)
+        val typeNamePyObject: NativePointer? = PyObject_GetAttrString(pointer, "__name__")
+
+        if (pyTypeObject != null && typeNamePyObject != null) {
+            val typeName: String = PyUnicode_AsUTF8(typeNamePyObject).let {it ?: throw NullPointerException("Failed to get type name") }
+            val result: Boolean = typeName == "type"
+
+            Py_DecRef(typeNamePyObject)
+            Py_DecRef(pyTypeObject)
+
+            return result
+        }
+
+        return false
+    }
 }
